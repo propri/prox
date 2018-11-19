a66910ba21505ece0f4c7f6d755831d6bfb56571
Wilhelm Mailaender <wilhelm.mailaender@sevenval.com>
2018-11-19 12:25:05 +0100

    tools berarbeitet

--- tools/aa.sh
+++ tools/aa.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 SEARCH="${1}"
+ALT_DIR="~/magic/inventory"
 
 ack -i "${SEARCH}"
 
-cd ~/magic/inventory
-
-ack -i "${SEARCH}"
+if [[ -d $ALT_DIR ]]
+then 
+	cd ~/magic/inventory
+	ack -i "${SEARCH}"
+fi
 
